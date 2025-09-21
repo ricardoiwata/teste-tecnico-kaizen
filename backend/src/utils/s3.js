ea8b21fab4821f63de5aa9e7a05aa0cb28c2db67
ricardoiwata <iwataricardo1@gmail.com>
2025-09-21 00:48:06 -0300

    chore(s3): cria utilitário para gerar pre-signed URL de upload

---
+++ backend/src/utils/s3.js
@@ -0,0 +1,22 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const s3 = new S3Client({ region: process.env.AWS_REGION });
+
+async function createPresignedPutURL({
+  bucket,
+  key,
+  contentType,
+  expiresIn = 60,
+}) {
+  const command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: key,
+    ContentType: contentType,
+    ACL: "public-read",
+  });
+  const url = await getSignedUrl(s3, command, { expiresIn });
+  return url;
+}
+
+module.exports = { s3, createPresignedPutURL };
